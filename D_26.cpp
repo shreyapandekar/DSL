a87f2783934dfa1396a03db4ed6bf28e2b3a0f53
shreyapandekar <113656443+shreyapandekar@users.noreply.github.com>
2022-12-08 16:04:18 +0530

    Update D_26.cpp

--- D_26.cpp
+++ D_26.cpp
@@ -1,128 +1,110 @@
-#include<iostream>
-#include<string.h>
-#include<stdlib.h>
+#include <iostream>
 using namespace std;
-#define size 10
+const int MAX = 50;
 
-class Expression{
+class stack{
+    char stck[MAX];
     int top;
-    char stack[size];
-public:
-    Expression()
-    {
-        top=-1;
-    }
-    void push(char);
-    char pop();
-    int isfull();
-    int isempty();
+    public:
+        stack(){
+			top = -1;
+		}
+        void push(char);
+        char pop();
+        int isFULL();
+        int isEMPTY();
+
 };
-void Expression::push(char x)
-{
-    top=top+1;
-    stack[top]=x;
-}
-char Expression::pop()
-{
-    char s;
-    s=stack[top];
-    top=top-1;
-    return s;
-}
-int Expression::isfull()
-{
-    if(top==size)
-        return 1;
-    else
-        return 0;
-}
-int Expression::isempty()
-{
-    if(top==-1)
-        return 1;
-    else
-        return 0;
-}
-int main()
-{
-    Expression s1;
-    char exp[20],ch;
-    int m=0;
-    cout<<"Parenthisis checker"<<endl;
-    cout<<"Enter the expression to check whether it is in will form or not;"<<endl;
-    cin>>exp;
-//     if((exp[0]==')')||(exp[0]==']')||(exp[0]=='}'))
-//     {
-//         cout<<"\n Invalid Expresson"<<endl;
-//         return 0;
-//     }
-//     else{
-//         while(exp[i]!='\0')
-//         {
-//             ch=exp[i];
-//             switch(ch)
-//             {
-//                 case '(':s1.push(ch);break;
-//                 case '[':s1.push(ch);break;
-//                 case '{':s1.push(ch);break;
-//                 case ')':s1.pop();break;
-//                 case ']':s1.pop();break;
-//                 case '}':s1.pop();break;
-//             }
-//             i=i+1;
-//         }
-//     }
-//     if(s1.isempty())
-//     {
-//         cout<<"\nExpression is well parenthesised...\n"<<endl;
-//     }
-//     else{
-//         cout<<"\nInvalid Expression or not in well parenthesizes..\n";
-//     }
-//     return 0;
-// }  
 
-    int len=strlen(exp);
-    for(int m=0;m<len;m++);
-    {
-        if(exp[m]=='{'|| exp[m]=='('|| exp[m]=='[')
-        {
-            char x;
-            x=exp[m];
-            s1.push(x);
-
-        }
-        if(exp[m]=='}'|| exp[m]==')' || exp[m]==']')
-        {
-            char x;
-            x=exp[m];
-            s1.pop();
- 
-        }
-        else{
-        while(exp[m]!='\0')
-        {
-            ch=exp[m];
-            switch(ch)
-            {
-                case '(':s1.push(ch);break;
-                case '[':s1.push(ch);break;
-                case '{':s1.push(ch);break;
-                case ')':s1.pop();break;
-                case ']':s1.pop();break;
-                case '}':s1.pop();break;
-            }
-            m=m+1;
-        }
-    }
-    if(s1.isempty())
-    {
-        cout<<"\nExpression is well parenthesised...\n"<<endl;
+int stack::isEMPTY(){
+    if (top == -1){
+        return 1;
     }
     else{
-        cout<<"\nInvalid Expression or not in well parenthesizes..\n";
+        return 0;
     }
+}
 
+int stack::isFULL(){
+    if(top == MAX - 1){
+        return 1;
     }
-    return 0;
+    else{
+        return 0;
+    }
+}
+void stack::push(char ch)
+{
+	if(!isFULL())
+	{
+		top++;
+		stck[top]=ch;
+	}
+}
+
+char stack::pop()
+{
+	if(!isEMPTY())
+	{
+		char ch=stck[top];
+		top--;
+		return ch;
+	}
+}
+int main(){
+	char opt;
+
+	cout<<"\n######  Expression Paranthesis Checker  ######\n";
+
+	do{
+		stack s;
+		char exp[20],ch,lch;
+		int i = 0,flag;
+		cout<<"\nEnter the Expression to Check :- ";
+		cin>>exp;
+		
+		while(exp[i] != '\0'){
+			ch = exp[i];
+			switch(ch){
+				case '(':s.push(ch);break;
+				case '{':s.push(ch);break;
+				case '[':s.push(ch);break;
+				case ')':
+					flag = 0;
+					lch = s.pop();
+					if (ch == ')' && lch == '('){
+						flag = 1;
+					}
+					break;
+				case '}':
+					flag = 0;
+					lch = s.pop();
+					if (ch == '}' && lch == '{'){
+						flag = 1;
+					}
+					break;
+				case ']':
+					flag = 0;
+					lch = s.pop();
+					if (ch == ']' && lch == '['){
+						flag = 1;
+					}
+					break;
+			}
+			i = i+1;
+		}
+		
+		if (s.isEMPTY() && flag == 1){
+			cout<<"\tExpression is parenthesised or in balanced form.\n";
+		}
+		else{
+			cout<<"\tExpression is not well parenthesised.  \n";
+		}
+
+		cout<<"\nDo you want to check other expression? (y/n) :- ";
+		cin>>opt;
+
+	}while(opt == 'y' || opt == 'Y');
+	
+	return 0;
 }
