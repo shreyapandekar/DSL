1ea81aeda4fccb14997144046a27c1fea78bb288
shreyapandekar <113656443+shreyapandekar@users.noreply.github.com>
2022-12-26 17:07:00 +0530

    Update A_1.py

--- A_1.py
+++ A_1.py
@@ -1,94 +1,54 @@
-C=[]
-B=[]
-F=[]
-U=[]
-c=int(input("enter number of students who play cricket"))
-for i in range(c):
-    cri=int(input("enter roll number"))
-    C.append(cri)
-print("students playing cricket:",C)
+students_play_cricekt = []
+students_play_badminton = []
+students_play_football = []
 
-b=int(input("enter number of students who play badminton"))
-for i in range(b):
-    bad=int(input("enter roll numbers who play badminton"))
-    B.append(bad)
-print("students playing badminton:",B)
+range_one = int(input("How many students play Cricket: "))
+for l in range(range_one):
+    name_cricket = str(input("Name of a Student to add in the list: "))
+    students_play_cricekt.append(name_cricket)
+print(f"Students who play cricket: {students_play_cricekt}\n\n")
 
-f=int(input("enter no of students who play football:"))
-for i in range(f):
-    foot=int(input("enter roll numbers who play football:"))
-    F.append(foot)
-print("students playing football:",F)
+range_two = int(input("How many students play Badminton: "))
+for m in range(range_two):
+    name_badminton = str(input("Name of a Student to add in the list: "))
+    students_play_badminton.append(name_badminton)
+print(f"Students who play badminton: {students_play_badminton}\n\n")
 
-U.extend(C)
-for i in B:
-    if i not in U:
-        U.append(i)
-for j in F:
-    if j not in U:
-        U.append(j)
-print("union of the students:",U)
+range_three = int(input("How many students play Football: "))
+for n in range(range_three):
+    name_football = str(input("Name of a Student to add in the list: "))
+    students_play_football.append(name_football)
+print(f"Students who play football: {students_play_football}\n\n")
 
-u=[]
-def cribad():
-    for i in C:
-        for j in B:
-            if i==j:
-                u.append(j)
-    print("students who play both cricket and badminton:",u)
-cribad()
+student_play_C_B = []
+for i in students_play_cricekt:
+    for j in students_play_badminton:
+        if i == j:
+            student_play_C_B.append(i)
+        else:
+            pass
+print(f"List of Students who play both Cricket and Badminton: {student_play_C_B}")
 
-m=[]
-def cri_bad_notboth():
-    for i in C:
-        if i not in B:
-            m.append(i)
-    for j in B:
-        if j not in C:
-            m.append(j)
+list_B = []
+a = students_play_cricekt
+b = students_play_badminton
+a, b = [i for i in a if i not in b], [j for j in b if j not in a]
+c = a + b
+list_B.append(c)
+print(f"List of Students who play either Cricket or Badminton but not both: {c}")
 
-    print("students who play either cricket or badminton but not both:",m)
-    return(m)
-'''cri_bad_notboth()'''
+list_C = []
+combined_list_one = list_B + student_play_C_B
+var = combined_list_one
+w = var
+x = students_play_football
+x = [j for j in x if j not in w]
+list_C.append(x)
+print(f"List of Students who play neither Cricket nor Badminton: {x}")
 
-def neither_cri_nor_bad():
-    cri_bad=[]
-    not_cri_bad=[]
-    cri_bad.extend(C)
-    for i in B:
-        if i not in C:
-            cri_bad.append(i)
-
-    for j in U:
-        if j not in cri_bad:
-            not_cri_bad.append(j)
-    print(not_cri_bad)
-'''neither_cri_nor_bad()'''
-
-def cri_foot_not_bad():
-    cri_foot=[]
-    not_bad=[]
-    cri_foot.extend(C)
-    for i in F:
-        if i not in C:
-            cri_foot.append(i)
-
-    for j in cri_foot:
-        if j not in B:
-            not_bad.append(j)
-    print(not_bad)
-'''cri_foot_not_bad()'''
-
-while(True):
-
-    ch=int(input("enter choice"))
-    if(ch==1):
-        print("cribad()")
-    if(ch==2):
-        print("list of students who play either cricket or badminton but not both:",cri_bad_notboth())
-    if(ch==3):
-        print("number of students who play neither cricket nor badminton:",neither_cri_nor_bad())
-    if(ch==4):
-        print("number of students who play cricket and football but not badminton:",cri_foot_not_bad())
-    if(ch==5):
-        exit()
+combined_list_two = students_play_cricekt + students_play_football
+p = combined_list_two
+q = students_play_badminton
+a = [i for i in p if i not in q]
+r = a
+print(f"List of Students who play Cricket and Football but not Badminton: {r}")
